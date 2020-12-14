f1e262c29081eafcb2a02c04a0c5112e59a576a1
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-14 22:02:16 +0530

    Added:Repeat till the player reaches the winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class SnakeAndLadder {
 
     static final int STARTING_POSITION = 0;
+    static  final int ENDING_POSITION = 100;
     static final int PLAYER=1;
     static final int LADDER = 1;
     static final int SNAKE = 2;
@@ -10,39 +11,45 @@ public class SnakeAndLadder {
 
     static Random RANDOM = new Random();
 
-    //uc2 rolling a random value to the player
+    //uc2 rolling the dice
     public int diceRoll()
     {
         int diceNumber = (RANDOM.nextInt(6)+1);
         System.out.println("dice number  " +diceNumber);
         return diceNumber;
     }
+    //uc3 gameplay
     public void gamePlay() {
         int playerPosition=STARTING_POSITION;
+
+    //uc4 Repeat till the Player reaches the winning position 100
+        while (playerPosition<ENDING_POSITION)
+        {
         int dice=diceRoll();
         int action = RANDOM.nextInt(3);
         System.out.println("action: "+action);
-        switch(action)
-        {
+            switch (action) {
 
-            case LADDER:
-                playerPosition = playerPosition + dice;
+                case LADDER:
+                    playerPosition = playerPosition + dice;
                     System.out.println("current ladder position: " + playerPosition);
-                break;
-            case SNAKE:
-                playerPosition = playerPosition - dice;
-                if(playerPosition<0){
-                    playerPosition=STARTING_POSITION;
-                }
-                System.out.println("current snake position: "+playerPosition);
-                break;
-            case NOPLAY:
-                playerPosition = playerPosition;
-                if(playerPosition<0){
-                    playerPosition=STARTING_POSITION;
-                }
-                System.out.println("no play position: "+playerPosition);
-                break;
+                    break;
+                case SNAKE:
+                    playerPosition = playerPosition - dice;
+                    if (playerPosition < 0) {
+                        playerPosition = STARTING_POSITION;
+                    }
+                    System.out.println("current snake position: " + playerPosition);
+                    break;
+                case NOPLAY:
+                    playerPosition = playerPosition;
+                    if (playerPosition < 0) {
+                        playerPosition = STARTING_POSITION;
+                    }
+                    System.out.println("no play position: " + playerPosition);
+                    break;
+            }
+            System.out.println(" ");
         }
     }
     public static void main(String args[])
