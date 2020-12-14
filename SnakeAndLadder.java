88e62df1042724ff7e519c6af7d26ddf6ee82d63
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-14 19:03:22 +0530

    Added:Roll the dice to get a number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,23 @@
+import java.util.Random;
+
 public class SnakeAndLadder {
 
     static final int STARTING_POSITION = 0;
+    static final int PLAYER=1;
+    static Random RANDOM = new Random();
+
+    //uc2 rolling a random value to the player
+    public void diceRoll()
+    {
+        int DICE = (RANDOM.nextInt(6)+1);
+        System.out.println("dice number  " +DICE);
+    }
+
     public static void main(String args[])
     {
-      System.out.println(STARTING_POSITION);
+        SnakeAndLadder snakeAndLadder=new SnakeAndLadder();
+        snakeAndLadder.diceRoll();
     }
+
+
 }
